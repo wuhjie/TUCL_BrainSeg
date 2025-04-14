13ce09d98be40ecaa5bdd069bfe02838dc15c35f
wuhjie <whjie001110@outlook.com>
2025-04-14 04:53:16 +0100

    update

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,7 @@ from .hausdorff_utils import cal_hd95, compute_hd95_single, hd95_score
 from .binary_utils import binary
 from .dataset_utils import get_dataloader, split_dataset
 from .data_preprocessing import load_npy_data, BraTS21Dataset
-from .train_utils import train, train_loop, val_loop
+# from .train_utils import train, train_loop, val_loop
 from .train_utils_tucl import train_loop_sp_uc
 from .train_utils_tucl import train_loop_sp
 from .train_utils_tucl import train_loop 
